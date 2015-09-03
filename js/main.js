47f4c79ee60de25467f4dbe042b7417ce5724ce3
Tom Scheduikat <tom.scheduikat@advocado.de>
2015-09-03 08:43:24 +0200

    anpassungen

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var header = $( ".header" );
+header.load('header.html', function() {
+    header.find('li.' + header.data('active')).addClass('active');
+}) ;
+$( ".footer" ).load('footer.html') ;
+
+$(document).on('ready', function() {
+
+    $.each($('div.section'), function(key, section){
+        var maxHeight = 0;
+        $.each($(section).find('div.images_1_of_3'), function(key2, column) {
+            if ($(column).height() > maxHeight) {
+                maxHeight = $(column).height();
+            }
+        });
+        $(section).find('div.images_1_of_3').height(maxHeight + 'px');
+    });
+});
+
